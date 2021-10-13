73a60c2b0929b4988e7edfcf22e5cedf1c82cafc
wilsonw13 <71456315+wilsonw13@users.noreply.github.com>
2021-10-13 00:07:07 -0400

    edited starting files

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,28 +1,60 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
 
 <style>
+:root {
+  --black: #131313;
+  --light-black: #232323;
+  --white: #fff;
+  --off-white: #f0f1f6;
+  --dark-grey: #1a2f41;
+  --grey: #252c30;
+  --lightGrey: #f2f4f6;
+  --background-primary: var(--light-black);
+  --skill-background: var(--black);
+  --card-background: var(--light-black);
+  --card-text: var(--white);
+  --background-secondary: var(--dark-grey);
+  --text-primary: var(--white);
+  --text-secondary: var(--black);
+  --modal-background: var(--black);
+  --modal-text: var(--white);
+  --student-text: var(--white);
+  --link-text: var(--white);
+  --nav-line: var(--white);
+  --header-btn: var(--white);
+  --footer-icon: var(--light-black);
+  --h1: 7.594rem;
+  --h2: 5.063rem;
+  --h3: 3.375rem;
+  --h4: 2.25rem;
+  --h5: 1.5rem;
+  --smallText: 0.8rem;
+  --containerPadding: 2.5%;
+  --headerHeight: 3rem;
+  --borderRadius: 10px
+}
+html {
+  box-sizing: border-box;
+  font-size: 62.5%;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-weight: 400;
+  line-height: 1.75;
+  background-color: var(--background-primary);
+  color: var(--text-primary);
+  margin: 0;
+  font-family: "Crimson Text",serif
 }
 </style>

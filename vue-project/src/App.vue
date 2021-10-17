9cdd9bc65542ad55708cfff28b5bb70425e69703
wilsonw13 <71456315+wilsonw13@users.noreply.github.com>
2021-10-16 20:48:49 -0400

    10/16 - Home

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -6,14 +6,13 @@
     <div class="body">
       <div class="left-container">
         <form id="search-bar" class="search-bar">
-          <input type="text" class="search-area" placeholder="Search Toppings"/>
-          <input type="button" class="search-button" value="Search"/>
+          <input type="text" class="search-area" placeholder="Search Toppings" v-model="searchQuery"/>
+          <input type="button" class="search-button" value="Search" @click="getSearchData(searchQuery)"/>
         </form>
         <div class="toppings-container">
-          <!-- <img v-for="(object) in responseArray"
+          <img v-for="object in responseArray"
             :key="object.id"
-            :id="object.id"
-            :src="object.src.original + '?auto=compress&cs=tinysrgb&q=75&ar=1:1&fit=crop'"> -->
+            :src="object.src.original + '?auto=compress&cs=tinysrgb&q=75&ar=1:1&fit=crop'">
         </div>
       </div>
       <div class="right-container">
@@ -33,12 +32,31 @@ export default {
   },
   data(){
     return{
-      responseArray: response.photos,
-      key: apiKey,
+      searchQuery: null,
+      previousQuery: null,
+      responseArray: null,
+      sampleResponse: response.photos,
+
     }
   },
   methods:{
-
+    async getSearchData (query = "pepperoni") {
+      if (query === this.previousQuery) {
+        console.log("Don't type the same thing!");
+        return;
+      }
+      try {
+        const response = await fetch(`https://api.pexels.com/v1/search?query=${query}&per_page=12`, {
+          headers: {"Authorization": apiKey},
+        });
+        const DataJSON = await response.json();
+        this.responseArray = DataJSON.photos;
+        this.previousQuery = query;
+      } catch (error) {
+        console.log(error);
+        // alert("Great Error Handling :D - An error as occured!");
+      }
+    },
   },
 }
 </script>
